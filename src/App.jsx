014379f104d4e95369cbc95115ac4203d01e7ec9
mdallamaiqbal <bd999plus13@gmail.com>
2026-03-15 15:48:44 +0600

    react world on the go

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Countries from './components/countries/countries'
+
+function App() {
+
+
+  return (
+    <>
+     <h1>React on the go...</h1>
+     <Countries></Countries>
+    </>
+  )
+}
+
+export default App
